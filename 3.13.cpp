b9f72918d0309285a0e5b8e637d32a553faf970e
HlebPlah <50776222+HlebPlah@users.noreply.github.com>
2019-09-14 13:54:51 +0300

    Create 3.13.cpp

---
+++ 3.13.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+	int sum=0;
+	
+                                                             
+	for (int i=1 ; i<=5; i++)
+	{
+		sum+=i*i;
+
+
+		
+	}
+	cout<<sum<<endl;
+	
+
+	
+	
+
+
+	
+	return 0;
+}
