dcce09614de6aeb9dc178f2cf67e12b00ee5cd89
HlebPlah <50776222+HlebPlah@users.noreply.github.com>
2019-09-14 13:46:00 +0300

    Create 1.1.cpp
    lab 2 , ex 1.1

---
+++ 1.1.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a,b;
+	a=b=0;
+	cout<<"enter num a : ";
+	cin>>a;
+	cout<<"enter num b : ";
+	cin>>b;
+	cout<<"a+b="<<a+b<<'\n';
+
+
+
+
+	
+	return 0;
+}
