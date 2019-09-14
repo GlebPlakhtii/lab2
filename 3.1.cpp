36bd50c448d70fcef962b98998b3b00c169504d9
HlebPlah <50776222+HlebPlah@users.noreply.github.com>
2019-09-14 13:52:46 +0300

    Create 3.1.cpp

---
+++ 3.1.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a;
+	cout<<"enter num to fnd multiplies : ";
+
+	cin>>a;
+
+
+	for (int i = 1; i<=int(300/a) ; i++)
+
+	{
+		cout<<a*i<<endl;
+
+	}
+
+
+	
+	
+
+
+	
+	return 0;
+}
