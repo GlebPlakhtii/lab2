36fcddc04beae300a7f860795db72e2679f94958
HlebPlah <50776222+HlebPlah@users.noreply.github.com>
2019-09-14 13:53:36 +0300

    Create 3.3.cpp

---
+++ 3.3.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+	int a1,d;
+	cout<<"enter A1 : ";
+	cin>>a1;
+	cout<<"enter d : ";
+	cin>>d;
+
+
+	for (int i=0; i<10; i++)
+	{
+
+		cout<<a1+d*i<<endl;
+	}
+	
+
+	
+	
+
+
+	
+	return 0;
+}
